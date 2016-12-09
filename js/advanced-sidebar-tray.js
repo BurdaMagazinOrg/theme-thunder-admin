a0d0b60df4288bc3374299052c452107dcbecf40
Andreas Sahle <andreas.sahle@wunderkraut.com>
2016-12-09 11:15:05 +0100

    sidebar-tray: proof of concept code to do some cross browser testing

---
+++ js/advanced-sidebar-tray.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Responsive advanced sidebar tray.
+ *
+ * This also supports collapsible navigable is the 'is-collapsible' class is
+ * added to the main element, and a target element is included.
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Initialise the tabs JS.
+   */
+  Drupal.behaviors.advancedSidebarTray = {
+    attach: function (context) {
+      var $body = $(context).find('body.advanced-sidebar-tray'),
+        $toggleBtn = $body.find('.form__advanced-toggle');
+      if ($body.length && $toggleBtn.length) {
+        $toggleBtn.unbind('click').on('click', function(e) {
+          e.preventDefault();
+          $body.toggleClass('advanced-sidebar-tray-open');
+        })
+      }
+    }
+  };
+
+})(jQuery, Drupal);
