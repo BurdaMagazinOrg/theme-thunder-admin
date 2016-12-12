2e30ca3d92d81ac4422ecc4e0a5d0e115d457fa3
Andreas Sahle <andreas.sahle@wunderkraut.com>
2016-12-12 17:13:43 +0100

    close toolbar menu tab if sidebar is toggled open and toolbar layout is vertical

--- js/advanced-sidebar-tray.js
+++ js/advanced-sidebar-tray.js
@@ -10,16 +10,23 @@
   'use strict';
 
   /**
-   * Initialise the tabs JS.
+   * Initialise the advanced sidebar tray JS.
    */
   Drupal.behaviors.advancedSidebarTray = {
     attach: function (context) {
       var $body = $(context).find('body.advanced-sidebar-tray');
-      var $toggleBtn = $body.find('.form__advanced-toggle');
+      // Add a click handler to the button(s) that toggle the advanced sidebar
+      // tray.
+      var $toggleBtn = $body.find('[data-toggle-advanced-sidebar-tray]');
       if ($body.length && $toggleBtn.length) {
         $toggleBtn.unbind('click').on('click', function (e) {
           e.preventDefault();
           $body.toggleClass('advanced-sidebar-tray-open');
+          // Close the vertical toolbar tab if the toolbar layout is vertical.
+          var $activeToolbarItem = $('.toolbar-item.is-active');
+          if ($body.hasClass('toolbar-vertical') && $activeToolbarItem.length) {
+            $activeToolbarItem.click();
+          }
         });
       }
     }
