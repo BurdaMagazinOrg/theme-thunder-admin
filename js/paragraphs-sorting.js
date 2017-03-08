327c911fac9dd22397c9b293cd489d102f6cd289
goldquest <tjwelde@gmail.com>
2017-03-08 18:04:52 +0100

    fixed eslint issues

--- js/paragraphs-sorting.js
+++ js/paragraphs-sorting.js
@@ -1,14 +1,13 @@
+(function ($, Drupal) {
 
-(function($, Drupal) {
+  'use strict';
 
-  'use strict'
-
-  Drupal.tableDrag.prototype.initCkbx = function() {
+  Drupal.tableDrag.prototype.initCkbx = function () {
     // add sorting toggle button on top
     this.$table.before($('<button type="button" class="tabledrag-toggle-checkbox"></button>')
-      .on('click', $.proxy(function(e) {
+      .on('click', $.proxy(function (e) {
         e.preventDefault();
-        this.toggleCheckboxes()
+        this.toggleCheckboxes();
       }, this))
       .text(Drupal.t('Sort'))
       .wrap('<div class="tabledrag-toggle-checkbox-wrapper"></div>')
@@ -19,43 +18,44 @@
     this.$table.find('.tabledrag-handle').after(
       $('<input type="checkbox" class="tabledrag-checkbox" />')
         .hide()
-    )
+    );
 
     // add sorting target
     var $target = $('<a href="#" class="tabledrag-sort-target">Sort here</a>')
-      .on('click', $.proxy(function(e) {
-        e.preventDefault()
-        alert('sorted')
-        console.log(e.target)
+      .on('click', $.proxy(function (e) {
+        e.preventDefault();
+        alert('sorted');
+        // console.log(e.target);
       }, this))
       .wrap('<div class="tabledrag-sort-target-wrapper"></div>')
       .parent()
-      .hide()
+      .hide();
 
     this.$table.find('.draggable')
       .after($target)
-      .first().before($target.clone(true))
+      .first().before($target.clone(true));
 
-  }
 
-  Drupal.tableDrag.prototype.toggleCheckboxes = function() {
-    this.$table.find('.tabledrag-handle').toggle()
-    this.$table.find('.tabledrag-checkbox').toggle()
-    this.$table.find('.tabledrag-sort-target-wrapper').toggle()
-  }
+  };
+
+  Drupal.tableDrag.prototype.toggleCheckboxes = function () {
+    this.$table.find('.tabledrag-handle').toggle();
+    this.$table.find('.tabledrag-checkbox').toggle();
+    this.$table.find('.tabledrag-sort-target-wrapper').toggle();
+  };
 
 
   Drupal.behaviors.paragraphs = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       for (var base in settings.tableDrag) {
         if (settings.tableDrag.hasOwnProperty(base)) {
-          var $table = $(context).find('#' + base).once('tabledrag-checkbox')
+          var $table = $(context).find('#' + base).once('tabledrag-checkbox');
           if ($table.length) {
-            Drupal.tableDrag[base].initCkbx()
+            Drupal.tableDrag[base].initCkbx();
           }
         }
       }
     }
-  }
+  };
 
-})(jQuery, Drupal)
+})(jQuery, Drupal);
