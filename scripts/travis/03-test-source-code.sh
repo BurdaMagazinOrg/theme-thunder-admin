4827e7461bdc4fb0c33da49f82f9899418806e8e
Timo Welde <tjwelde@gmail.com>
2017-08-16 15:51:01 +0200

    configure phpcs and eslint correctly

--- scripts/travis/03-test-source-code.sh
+++ scripts/travis/03-test-source-code.sh
@@ -2,14 +2,15 @@
 # run phpcs
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 # Ignore check of .md files, because they should be able to contain more then 80 characters per line.
-phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=*.md -p .
+phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=css/* -p .
 # Ignore *.yml file because the enforced usage of prefixed dependencies eems to be not working.
-phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme --ignore=thunder.info.yml -p .
+phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme -p .
 
 # JS ESLint checking
 set -x
 source ~/.nvm/nvm.sh
 set +x
 nvm install 6
-npm install -g eslint
-eslint .
+npm install -g yarn
+yarn --frozen-lockfile --ignore-scripts
+npm run js
