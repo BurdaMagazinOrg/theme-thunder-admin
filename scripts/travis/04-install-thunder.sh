c0eed0c58369df25c17edc4f8fc024ca8f53ae47
Timo Welde <tjwelde@gmail.com>
2017-08-23 16:34:11 +0200

    Install demo content

--- scripts/travis/04-install-thunder.sh
+++ scripts/travis/04-install-thunder.sh
@@ -6,7 +6,7 @@
 install_thunder() {
     cd $1
 
-    /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder --account-pass=1234 --db-url=mysql://thunder:thunder@127.0.0.1/drupal -y thunder_module_configure_form.install_modules_thunder_demo
+    /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder --account-pass=1234 --db-url=mysql://thunder:thunder@127.0.0.1/drupal -y
 }
 
 # Update thunder to current test version
