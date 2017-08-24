5e07877404a2fcad06da10528edcc16593a7ff31
Timo Welde <tjwelde@gmail.com>
2017-08-24 13:41:15 +0200

    ignore markdown files while testing source code, because they should be able to contain more then 80 chars per line

--- scripts/travis/03-test-source-code.sh
+++ scripts/travis/03-test-source-code.sh
@@ -2,7 +2,7 @@
 # run phpcs
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 # Ignore check of .md files, because they should be able to contain more then 80 characters per line.
-phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=css/* -p .
+phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore="css/*,*.md" -p .
 # Ignore *.yml file because the enforced usage of prefixed dependencies eems to be not working.
 phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme -p .
 
