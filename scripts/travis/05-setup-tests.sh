52dd9d7edd3fd8b3c8d55e1fe6cc7a2944ad31ae
Mladen Todorovic <mladen.todorovic@burda.com>
2017-11-28 15:52:28 +0100

    Added new screenshot and also added functionality to pre-generate image styles

--- scripts/travis/05-setup-tests.sh
+++ scripts/travis/05-setup-tests.sh
@@ -6,6 +6,13 @@ cd ${TEST_DIR}/docroot
 # Final cache rebuild, to make sure every code change is respected
 drush cr
 
+# Pre-create all image styles for entity browser. ${FILE_NAME:20} will remove "sites/default/files/" from path.
+# Execution of this code is based when bash is in "docroot" folder.
+for FILE_NAME in sites/default/files/2016-05/*
+do
+    drush image-derive entity_browser_thumbnail public://${FILE_NAME:20}
+done
+
 # Run the webserver
 drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
 
