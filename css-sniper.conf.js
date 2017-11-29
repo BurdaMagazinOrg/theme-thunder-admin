4b57a2fde214e73610e99b40594fba2858cf6e08
Volker Killesreiter <killesre@gmail.com>
2017-11-29 13:31:03 +0100

    semicolon

--- css-sniper.conf.js
+++ css-sniper.conf.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 module.exports = {
   resolver: function (base) {
-    const [type, name] = base.split('-')
+    const [type, name] = base.split('-');
     if (!type || !name) return null;
     return execSync(`drush eval "echo DRUPAL_ROOT . '/'. drupal_get_path('${type}', '${name}');"`).toString('utf8');
   }
