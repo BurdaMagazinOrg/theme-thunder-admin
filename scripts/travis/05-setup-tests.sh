a401bcac4a1155eced271d6a79b422cfa0702c6f
Mladen Todorovic <mladen.todorovic@burda.com>
2017-11-30 15:54:16 +0100

    Adjusted documentation, image style creation and Chrome docker shared memory

--- scripts/travis/05-setup-tests.sh
+++ scripts/travis/05-setup-tests.sh
@@ -8,10 +8,7 @@ drush cr
 
 # Pre-create all image styles for entity browser. ${FILE_NAME:20} will remove "sites/default/files/" from path.
 # Execution of this code is based when bash is in "docroot" folder.
-for FILE_NAME in sites/default/files/2016-05/*
-do
-    drush image-derive entity_browser_thumbnail public://${FILE_NAME:20}
-done
+drush --include=themes/contrib/thunder_admin/scripts/drush seed-image-derivatives --include=all
 
 # Run the webserver
 drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
@@ -19,10 +16,10 @@ drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
 # Run Selenium2 server with Browser relevant for running environment
 if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
     docker pull selenium/standalone-chrome:3.4.0-einsteinium
-    docker run -d -p 4444:4444 -v /dev/shm:/dev/shm --net=host selenium/standalone-chrome:3.4.0-einsteinium
+    docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-chrome:3.4.0-einsteinium
 elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
     docker pull selenium/standalone-firefox:3.4.0-einsteinium
-    docker run -d -p 4444:4444 -v /dev/shm:/dev/shm --net=host selenium/standalone-firefox:3.4.0-einsteinium
+    docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-firefox:3.4.0-einsteinium
 fi
 
 docker ps -a
