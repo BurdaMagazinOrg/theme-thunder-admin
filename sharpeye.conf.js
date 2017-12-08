b71d118e67d45a5588bb6ce1175d35b447a2cdb3
Volker Killesreiter <killesre@gmail.com>
2017-12-08 17:09:27 +0100

    recompiled css

--- sharpeye.conf.js
+++ sharpeye.conf.js
@@ -1,11 +1,11 @@
 // Options for sharpeye
 exports.options = {
   // The base URL of the website.
-  baseUrl: 'http://docker:8080',
+  baseUrl: 'http://dev2.dd:8083',
   // Username of admin user.
   user: 'admin',
   // Password of admin user.
-  pass: '1234',
+  pass: 'admin',
   // Specify the mismatch tolerance of the comparison.
   misMatchTolerance: 0.04
 }
@@ -28,13 +28,24 @@ exports.config = {
   // sessions. Within your capabilities you can overwrite the spec and exclude options in
   // order to group specific specs to a specific capability.
   //
+  // https://sites.google.com/a/chromium.org/chromedriver/capabilities
+  // https://github.com/mozilla/geckodriver/blob/master/README.md#webdriver-capabilities
   //
   capabilities: [
+    /*
     {
-      browserName: 'firefox'
-    },
+      browserName: 'chrome',
+      "chromeOptions": {
+        "args": [
+          "window-size=1280,800",
+          "hide-scrollbars"
+        ]
+      }
+    }
+    */
     {
       browserName: 'chrome'
     }
-  ]
+  ],
+  port: 5544
 };
