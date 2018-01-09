47326ff0db77586c0f97c0ee8cff9b580f3821f7
Volker Killesreiter <killesre@gmail.com>
2018-01-09 15:22:36 +0100

    Issue #2934140: Missing development dependencies (#102)
    * add dev dev dependencies on eslint
    
    * Rename paste.svg file (noticed on Linux)
    
    * Adjust that .eslintrc is root for this project
    
    * Added documentation for css-sniper configuration
    
    * do not run browsersync on install

--- css-sniper.conf.js
+++ css-sniper.conf.js
@@ -1,9 +1,24 @@
 const { execSync } = require('child_process');
+const { existsSync } = require('fs');
 
 module.exports = {
+  fallbackThemesPath: '../../../core/themes/',
   resolver: function (base) {
     const [type, name] = base.split('-');
     if (!type || !name) return null;
-    return execSync(`drush eval "echo DRUPAL_ROOT . '/'. drupal_get_path('${type}', '${name}');"`).toString('utf8');
+
+    try {
+      return execSync(`drush eval "echo DRUPAL_ROOT . '/'. drupal_get_path('${type}', '${name}');"`).toString('utf8');
+    }
+    catch (err) {
+      if (type !== 'theme') return null;
+
+      // As fallback it's expected that Thunder Admin Theme is in directory: "[docroot]/themes/contrib/thunder_admin"
+      const fallbackPath = this.fallbackThemesPath + '/' + name;
+      if (!existsSync(fallbackPath)) return null;
+
+      return fallbackPath;
+    }
+
   }
 }
