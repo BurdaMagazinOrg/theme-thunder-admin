f4d61f206b859efe0a9e91a4c5b2cdeeb8d1e962
Volker Killesreiter <killesre@gmail.com>
2018-03-09 09:50:38 +0100

    imports swapped

--- css-sniper.conf.js
+++ css-sniper.conf.js
@@ -2,10 +2,16 @@ const { execSync } = require('child_process');
 const { existsSync } = require('fs');
 
 module.exports = {
-  fallbackThemesPath: '../../../core/themes/',
+  assumedThemesPath: '../../../core/themes/',
   resolver: function (base) {
     const [type, name] = base.split('-');
     if (!type || !name) return null;
+
+    // Return expected directory. Thunder Admin Theme is in directory: "[docroot]/themes/contrib/thunder_admin"
+    const assumedPath = this.assumedThemesPath + '/' + name;
+    if (existsSync(assumedPath)) {
+      return assumedPath;
+    }
 
     try {
       return execSync(`drush eval "echo DRUPAL_ROOT . '/'. drupal_get_path('${type}', '${name}');"`).toString('utf8');
@@ -13,12 +19,8 @@ module.exports = {
     catch (err) {
       if (type !== 'theme') return null;
 
-      // As fallback it's expected that Thunder Admin Theme is in directory: "[docroot]/themes/contrib/thunder_admin"
-      const fallbackPath = this.fallbackThemesPath + '/' + name;
-      if (!existsSync(fallbackPath)) return null;
-
-      return fallbackPath;
+      console.log('Could not determine directory for ${type} ${name}'); 
+      return null;
     }
-
   }
 }
