1be13ef68cd1da6002761be431d50f453dfa99cb
Volker Killesreiter <killesre@gmail.com>
2018-06-06 14:29:48 +0200

    fixed remarks from mtodor

--- css-sniper.conf.js
+++ css-sniper.conf.js
@@ -4,43 +4,41 @@ const { existsSync } = require('fs');
 module.exports = {
   candidateThemePaths: [
     '../../../themes/contrib',
-    '../../../core/themes'
+    '../../../core/themes',
   ],
   candidateModulePaths: [
     '../../../modules/contrib',
-    '../../../core/modules'
+    '../../../core/modules',
   ],
-  resolver: function (base) {
+  resolver(base) {
     const [type, name] = base.split('-');
     if (!type || !name) return null;
-    let candidatePaths, foundPath;
+    let candidatePaths = [];
 
-    // Return expected directory. Thunder Admin Theme is in directory: "[docroot]/themes/contrib/thunder_admin"
+    // Return expected directory. Thunder Admin Theme is in directory:
+    // "[docroot]/themes/contrib/thunder_admin"
     if (type === 'theme') {
       candidatePaths = this.candidateThemePaths;
     }
     else if (type === 'module') {
       candidatePaths = this.candidateModulePaths;
     }
-
-    candidatePaths.forEach((modulePath) => {
-      const candidatePath = modulePath + '/' + name;
-      if (existsSync(candidatePath)) {
-        foundPath = candidatePath;
+    /* eslint-disable-next-line no-restricted-syntax */
+    for (const candidatePath of candidatePaths) {
+      const path = `${candidatePath}/${name}`;
+      if (existsSync(path)) {
+        return path;
       }
-    });
-
-    if (foundPath) return foundPath;
+    }
 
     try {
       return execSync(`drush eval "echo DRUPAL_ROOT . '/'. drupal_get_path('${type}', '${name}');"`).toString('utf8');
     }
     catch (err) {
-      if (type !== 'theme') return null;
-      /* eslint no-console: "error" */
-      console.log('Could not determine directory for ${type} ${name}');
+      /* eslint-disable-next-line no-console */
+      console.log(`Could not determine directory for ${type} ${name}`);
       return null;
     }
-  }
+  },
 };
 
