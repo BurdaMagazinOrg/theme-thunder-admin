cc3ec8db6bdca476d25b1f9ca342e2b0a1b80e40
Volker Killesreiter <killesre@gmail.com>
2019-04-30 18:06:58 +0200

    re-order tests

--- sharpeye.tasks.js
+++ sharpeye.tasks.js
@@ -190,17 +190,17 @@ module.exports = [
     { $: '[data-drupal-selector="edit-displays-settings-settings-content-tab-content-details-columns-third-arguments"] .views-ui-display-tab-setting a.views-ajax-link', offset: -150, wait: '[data-drupal-selector="edit-options-argument-present"]' },
     { $: '[data-drupal-selector="edit-options-form-description"] ', waitBefore: 1000 }
   ]},
-  { name: 'Views overlay and toolbar', path: '/admin/structure/views/view/content', actions: [
-    { $: '#toolbar-item-administration-tray > nav > div.toolbar-toggle-orientation > div > button', offset: -150  },
-    { $: '[data-drupal-selector="edit-displays-settings-settings-content-tab-content-details-columns-third"]', offset: -150 },
-    { $: '[data-drupal-selector="edit-displays-settings-settings-content-tab-content-details-columns-third-relationships"] .views-ui-display-tab-setting a.views-ajax-link', offset: -150, wait: '[data-drupal-selector="edit-options-required"]' },
-    { $: '.preview-section tbody .views-field-changed', replace: '01/01/2018 - 00:00' }
-  ]},
   { name: 'Show description on form error', path: '/admin/structure/types/manage/article/form-display', actions: [
     { $: '//input[@data-drupal-selector="edit-fields-field-tags-settings-edit"]', offset: -150 },
     { $: '//input[@data-drupal-selector="edit-fields-field-tags-settings-edit-form-settings-width"]', fill: "abc", waitBefore: 1000 },
     { $: '//input[@data-drupal-selector="edit-fields-field-tags-settings-edit-form-actions-save-settings"]', offset: -150 },
     { $: '//div[@data-drupal-selector="edit-fields-field-tags-settings-edit-form"]', waitBefore: 1000 },
+  ]},
+  { name: 'Views overlay and toolbar', path: '/admin/structure/views/view/content', actions: [
+    { $: '#toolbar-item-administration-tray > nav > div.toolbar-toggle-orientation > div > button', offset: -150  },
+    { $: '[data-drupal-selector="edit-displays-settings-settings-content-tab-content-details-columns-third"]', offset: -150 },
+    { $: '[data-drupal-selector="edit-displays-settings-settings-content-tab-content-details-columns-third-relationships"] .views-ui-display-tab-setting a.views-ajax-link', offset: -150, wait: '[data-drupal-selector="edit-options-required"]' },
+    { $: '.preview-section tbody .views-field-changed', replace: '01/01/2018 - 00:00' }
   ]},
   { name: 'Resize tabs', path: '/admin/structure/types/manage/article/display', viewports: [{width: 400, height: 800}], hide: [
     '.form-item-fields-field-channel-type',
