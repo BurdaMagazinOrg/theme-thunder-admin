6f900b399e6fb2418474d7d0d43cdc7f2d8fb7af
Volker Killesreiter <killesre@gmail.com>
2020-02-06 09:50:04 +0100

    add 0.1% tolerance

--- sharpeye.conf.js
+++ sharpeye.conf.js
@@ -9,7 +9,7 @@ exports.options = {
   editorUser: 'test-editor',
   editorPass: 'test-editor',
   // Specify the mismatch tolerance of the comparison.
-  misMatchTolerance: 0
+  misMatchTolerance: 0.1
 };
 
 var jobID = process.env.TRAVIS_JOB_ID;
