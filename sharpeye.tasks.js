bdc0600b9acd511918fa36b2c274f3c87fd1e72f
Volker Killesreiter <killesre@gmail.com>
2020-03-30 17:46:08 +0200

    add stupid wait

--- sharpeye.tasks.js
+++ sharpeye.tasks.js
@@ -184,7 +184,7 @@ module.exports = [
     { fill: [
       { $: 'input.select2-search__field', value: "abc" }
     ]},
-    { wait: 'li.select2-results__option' }
+    { waitBefore: 1000 }
   ]},
   { name: 'Select2 selection', path: '/node/7/edit', fullPage: true, replace: [
     { $: '//*[@id="edit-meta-changed"]/text()', value: ' 01/01/2018 - 00:00' }
