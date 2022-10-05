60c9f923954deec12aebb028cb595b9c66f5add4
Volker Killesreiter <killesre@gmail.com>
2022-10-05 10:25:20 +0200

    fix ff

--- sharpeye.tasks.js
+++ sharpeye.tasks.js
@@ -72,6 +72,7 @@ module.exports = [
   ]},
   {name: 'CKEditor dialog', path: '/edit/node/36b2e2b2-3df0-43eb-a282-d792b0999c07', element: '.cke_dialog', actions: [
     {$: '[data-drupal-selector="field-paragraphs-1-edit-2"]', wait: '.paragraph-form-item--has-subform'},
+    {$: '[data-drupal-selector="edit-field-paragraphs-1-subform-field-text-0-format"]'},
     {$: '//select[@data-drupal-selector="edit-field-paragraphs-1-subform-field-text-0-format"]/option[@value=\'full_html\']'},
     {$: '//div[contains(@class,"editor-change-text-format-modal")]/div[3]/div/button[1]', wait: 'div[id^=cke_edit-field-paragraphs-1-subform-field-text-0-value]'},
     {$: '//*[contains(@class,"cke_button_off") and @title="Table"]'},
